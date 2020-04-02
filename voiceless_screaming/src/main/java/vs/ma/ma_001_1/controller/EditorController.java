116ed79768a1aa6d933532d18914ec6cce3cd52c
dev_krk <use770@naver.com>
2020-04-02 11:44:28 +0900

    ㅏㅏ

---
+++ voiceless_screaming/src/main/java/vs/ma/ma_001_1/controller/EditorController.java
@@ -0,0 +1,17 @@
+package vs.ma.ma_001_1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/edit/*")
+public class EditorController {
+
+	@RequestMapping("/edit_page")
+	public void edit() {
+		log.info("edit--------------------");
+	}
+}
