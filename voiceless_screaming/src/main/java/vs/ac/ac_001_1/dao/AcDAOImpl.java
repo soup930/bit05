6e51ca7421f09d2056909ade5b9a0ff6fe1113bb
dev_krk <use770@naver.com>
2020-04-06 12:39:37 +0900

    되라잇

---
+++ voiceless_screaming/src/main/java/vs/ac/ac_001_1/dao/AcDAOImpl.java
@@ -0,0 +1,34 @@
+package vs.ac.ac_001_1.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.stream.events.Namespace;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+ 
+import vs.ac.ac_001_1.domain.AcVO;
+ 
+@Repository
+public class AcDAOImpl implements AcDAO{
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	
+	@Override
+	public List<AcVO> Ac_List() {
+		
+		System.out.println("¿©±â´Â DAOImpl");
+		
+		List<AcVO> list = sqlSession.selectList("ac.ac_list");
+		
+		System.out.println(list + "dao : list");
+		
+		return list;
+		
+	}
+	
+}
